e6787fa98e1c9fbb7e07119fec0b5439bca5a5ff
HP <HP@DESKTOP-K32KFOQ.mshome.net>
2025-11-26 16:19:41 +0530

    S

--- Supframework/src/test/java/testpack/Super.java
+++ Supframework/src/test/java/testpack/Super.java
@@ -9,7 +9,7 @@ public class Super {
 		System.out.println("done");
 		System.out.println("HI");
 		System.out.println("BYE");
-		System.out.println("WHY");
+		System.out.println("WHY SDF");
 	}
 
 }
