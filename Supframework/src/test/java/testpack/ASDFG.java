369194c060abe04e7b35d9774536e2c000c0b158
HP <HP@DESKTOP-K32KFOQ.mshome.net>
2025-11-26 16:24:42 +0530

    DF

---
+++ Supframework/src/test/java/testpack/ASDFG.java
@@ -0,0 +1,17 @@
+package testpack;
+
+import org.testng.annotations.Test;
+
+public class ASDFG {
+	@Test
+	public void sup()
+	{
+		System.out.println("doneSDF");
+		System.out.println("HIDS");
+		System.out.println("BYED");
+		System.out.println("WHY SDFD");
+		
+	}
+
+
+}
